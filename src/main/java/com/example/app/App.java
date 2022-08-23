53dcef31b86d790db32ad56df6021709a2a7e165
Eugene <zhenyaesipov@mail.ru>
2022-08-23 14:15:39 +0400

    wallpaper changer

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,4 +1,6 @@
-public class Main {
+package com.example.app;
+
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
---
+++ src/main/java/com/example/app/wallpaper/changer/WallpaperChanger.java
@@ -0,0 +1,18 @@
+package com.example.app.wallpaper.changer;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.win32.W32APIOptions;
+
+public class WallpaperChanger {
+
+    public static boolean change(String path) {
+        return User32.INSTANCE.SystemParametersInfo(0x0014, 0, path, 1);
+    }
+
+    private interface User32 extends Library {
+        User32 INSTANCE = Native.load("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
+
+        boolean SystemParametersInfo(int uiAction, int uiParam, String pvParam, int fWinIni);
+    }
+}
